0464c83c8b07ef74ae574e2717de4770916ac399
Guillermo dam <gremesalgarcia@safareyes.es>
2024-10-01 14:07:32 +0200

    porsiacaso

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,7 @@ import {
   IonContent,
   IonCard,
   IonCardHeader,
-  IonCardTitle, IonCardSubtitle, IonCardContent
+  IonCardTitle, IonCardSubtitle, IonCardContent, IonSearchbar, IonList, IonItem, IonLabel, IonImg
 } from '@ionic/angular/standalone';
 import {NavbarComponent} from "../navbar/navbar.component";
 import {InicioSesionComponent} from "../inicio-sesion/inicio-sesion.component";
@@ -26,9 +26,32 @@ import {InicioSesionComponent} from "../inicio-sesion/inicio-sesion.component";
     IonCardTitle,
     IonCardSubtitle,
     IonCardContent,
-    InicioSesionComponent
+    IonSearchbar,
+    IonList,
+    IonItem,
+    IonLabel,
+    IonImg,
   ],
 })
 export class HomePage {
   constructor() {}
+  public data = [
+    'Amsterdam',
+    'Buenos Aires',
+    'Cairo',
+    'Geneva',
+    'Hong Kong',
+    'Istanbul',
+    'London',
+    'Madrid',
+    'New York',
+    'Panama City',
+  ];
+  public results = [...this.data];
+
+  handleInput(event: any) {
+    console.log(event)
+    const query = event.target.value.toLowerCase();
+    this.results = this.data.filter((d) => d.toLowerCase().indexOf(query) > -1);
+  }
 }
