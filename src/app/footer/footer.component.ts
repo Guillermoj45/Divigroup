4b1733aa70f531f92dcd9fc45b163d08c2865956
Guillermo dam <gremesalgarcia@safareyes.es>
2024-10-03 14:14:29 +0200

    Creación de footer y optimizaciones sobre home.page.ts

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,27 +1,45 @@
 import { Component, OnInit } from '@angular/core';
-import {IonButton, IonButtons, IonFooter, IonIcon, IonToolbar} from "@ionic/angular/standalone";
+import {
+  IonButton,
+  IonButtons,
+  IonCol,
+  IonFooter,
+  IonGrid,
+  IonIcon,
+  IonRouterLink,
+  IonRow,
+  IonToolbar
+} from "@ionic/angular/standalone";
 import {addIcons} from "ionicons";
-import {addOutline, document} from "ionicons/icons";
+import {addOutline, document, people, peopleOutline, person} from "ionicons/icons";
+import {Routes, RouterModule} from "@angular/router"
 
 @Component({
     selector: 'app-footer',
     templateUrl: './footer.component.html',
     styleUrls: ['./footer.component.scss'],
     standalone: true,
-    imports: [
-        IonButton,
-        IonButtons,
-        IonFooter,
-        IonIcon,
-        IonToolbar
-    ]
+  imports: [
+    IonButton,
+    IonButtons,
+    IonFooter,
+    IonIcon,
+    IonToolbar,
+    IonGrid,
+    IonCol,
+    IonRow,
+    IonRouterLink
+  ]
 })
 export class FooterComponent  implements OnInit {
 
   constructor() {
-    addIcons({'add-outline': addOutline, 'document': document});
+    addIcons({'add-outline': addOutline, 'document': document, 'people': people, 'person': person});
   }
-
+  routes: Routes = [
+    { path: 'hola', component: HolaComponent },
+    // otras rutas
+  ];
   ngOnInit() {}
 
 }
