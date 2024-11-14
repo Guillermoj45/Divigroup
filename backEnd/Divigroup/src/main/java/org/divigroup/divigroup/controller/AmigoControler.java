f2a85ba467fe9073a23cc3b5c146f0a5ff6fbb34
Guillermo <guillermoremesa@gmail.com>
2024-11-14 13:37:57 +0100

    Pantalla amigos hecha

--- backEnd/Divigroup/src/main/java/org/divigroup/divigroup/controller/AmigoControler.java
+++ backEnd/Divigroup/src/main/java/org/divigroup/divigroup/controller/AmigoControler.java
@@ -3,10 +3,7 @@ package org.divigroup.divigroup.controller;
 import org.divigroup.divigroup.model.Usuario;
 import org.divigroup.divigroup.service.AmigoService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -17,8 +14,8 @@ public class AmigoControler {
     @Autowired
     private AmigoService amigoService;
 
-    @GetMapping("/{idUsuario}")
-    public List<Usuario> listarAmigos(@PathVariable int idUsuario){
+    @GetMapping()
+    public List<Usuario> listarAmigos(@CookieValue(value = "idUsuario") int idUsuario){
         return amigoService.amigosUsuario(idUsuario);
     }
 }
